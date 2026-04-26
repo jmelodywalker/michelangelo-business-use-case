ffacda5f3443857a8c8d1be9233c104d3fc80339
jmelodywalker <104225344+jmelodywalker@users.noreply.github.com>
2026-04-26 14:50:31 -0700

    Update vite.config.ts
    Adding  base: '/michelangelo-business-use-case/', to match repo main

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// Set `base` to your repo name when deploying to GitHub Pages:
-//   https://<username>.github.io/michelangelo-field-notes/
-// If you serve from a custom domain root, change `base` to '/'.
 export default defineConfig({
   plugins: [react()],
-  base: '/michelangelo-field-notes/',
-});
+  base: '/michelangelo-business-use-case/',
+})
